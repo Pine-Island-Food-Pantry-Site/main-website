0af026970fcd372407ab7366989438db1d834e14
Peter Cruckshank <petecruckshank@gmail.com>
2024-02-28 14:19:02 -0500

    ✨ new page for /posts to fetch and display posts with links to them.

--- app/(personal)/posts/page.tsx
+++ app/(personal)/posts/page.tsx
@@ -1,10 +1,12 @@
 import { toPlainText } from '@portabletext/react'
-import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { PostPageProps } from '@/components/pages/post/PostPage'
 import { allPostsQuery } from '@/sanity/lib/queries'
+import { urlForImage } from '@/sanity/lib/utils'
 
 import { client } from '../../../sanity/lib/client'
+import styles from '../../../styles/posts.module.css'
 
 interface Post {
   title: string
@@ -13,10 +15,6 @@ interface Post {
   description: string
   overview: string
   slug: string
-}
-
-interface Props {
-  posts: Post[]
 }
 
 async function getPosts() {
@@ -43,36 +41,43 @@ async function getPosts() {
 
 export default async function PostsPage() {
   const posts = await getPosts()
-  console.log(toPlainText(posts[0].overview))
+
   return (
-    <div>
-      <h1>All Posts</h1>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <h2>{post.title}</h2>
-          <p>{toPlainText(post.overview)}</p>
-        </div>
-      ))}
+    <div className={styles.posts_page}>
+      <h1>Welcome to The Pine Island Food Pantry</h1>
+      <h2>Providing food assistance for those in need.</h2>
+      <Image
+        src={'logo.svg'}
+        alt="Pine Island Food Pantry Logo"
+        width={200}
+        height={200}
+      />
+      <a href="https://maps.app.goo.gl/DTgw8M5Lgby9eFvY6">
+        12175 Stringfellow Road Bokeelia, FL 33922 USA
+      </a>
+      <a href="#">Donate</a>
+      <h2>Our Posts</h2>
+      {posts.map((post, index) => {
+        const imageUrl =
+          post.coverImage &&
+          urlForImage(post.coverImage)?.height(250).width(250).fit('crop').url()
+
+        return (
+          <div key={index} className={styles.card}>
+            <div className={styles.card_image}>
+              <Image
+                src={imageUrl}
+                alt={post.title}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <h3>{post.title}</h3>
+            <p>{toPlainText(post.overview).substring(0, 155)}</p>
+            <Link href={`/posts/${post.slug}`}>Read More</Link>
+          </div>
+        )
+      })}
     </div>
   )
 }
-
-// export const getStaticProps: GetStaticProps<Props> = async () => {
-//   // Fetch posts from Sanity
-//   const response = await client.fetch(allPostsQuery)
-
-//   const posts: Post[] = response.map((post: any) => ({
-//     title: post?.title,
-//     _id: post._id,
-//     coverImage: post.coverImage,
-//     description: toPlainText(post.overview),
-//     overview: post.overview,
-//     slug: post.slug.current,
-//   }))
-
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
