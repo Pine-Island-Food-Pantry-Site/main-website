e409e2e5cdd1a4f8f31a898786581ecf4d52c76e
Peter Cruckshank <petecruckshank@gmail.com>
2024-04-24 14:24:19 -0400

    💄 Update UI for single post page

--- components/pages/post/PostPage.tsx
+++ components/pages/post/PostPage.tsx
@@ -1,10 +1,13 @@
 import type { EncodeDataAttributeCallback } from '@sanity/react-loader'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { CustomPortableText } from '@/components/shared/CustomPortableText'
 import { Header } from '@/components/shared/Header'
 import ImageBox from '@/components/shared/ImageBox'
 import type { PostPayload } from '@/types'
+
+import styles from '../../../styles/single_post.module.css'
 
 export interface PostPageProps {
   data: PostPayload | null
@@ -22,60 +25,78 @@ export function PostPage({ data, encodeDataAttribute }: PostPageProps) {
 
   return (
     <div>
-      <div className="mb-20 mt-5 space-y-6 px-16">
-        {/* Header */}
-        <Header title={title} description={overview} />
-
-        <div className="rounded-md border">
-          {/* Image  */}
-          <ImageBox
-            data-sanity={encodeDataAttribute?.('coverImage')}
-            image={coverImage}
-            // @TODO add alt field in schema
-            alt=""
-            classesWrapper="relative aspect-[16/9]"
+      <div className={styles.page}>
+        <h1 className={styles.h1_text}>
+          Welcome to The Pine Island Food Pantry
+        </h1>
+        <h2 className={styles.h2_text}>
+          Providing food assistance for those in need.
+        </h2>
+        <div className={styles.image_div}>
+          <Image
+            src={'/logo.svg'}
+            alt="Pine Island Food Pantry Logo"
+            fill={true}
+            sizes="(max-width: 768px) 75vw, (max-width: 1200px) 50vw"
           />
+        </div>
+        <a
+          href="https://maps.app.goo.gl/DTgw8M5Lgby9eFvY6"
+          className={styles.top_link}
+        >
+          12175 Stringfellow Road Bokeelia, FL 33922 USA
+        </a>
+        <a
+          className={styles.donate_button}
+          href="https://www.paypal.com/donate?hosted_button_id=45JBRR8VRXJ76"
+          target="_blank"
+        >
+          Donate
+        </a>
+        <div className={`food_background ${styles.outer_card}`}>
+          <div className={styles.inner_card}>
+            <div className={styles.image_container}>
+              {/* Image  */}
+              <ImageBox
+                data-sanity={encodeDataAttribute?.('coverImage')}
+                image={coverImage}
+                // @TODO add alt field in schema
+                alt=""
+                classesWrapper={styles.image_container}
+              />
 
-          <div className="divide-inherit grid grid-cols-1 divide-y lg:grid-cols-4 lg:divide-x lg:divide-y-0">
-            {/* Publish Date */}
-            {!!year && (
-              <div className="p-3 lg:p-4">
-                <div className="text-xs md:text-sm">Publish Date</div>
-                <div className="text-md md:text-lg">
-                  <span
-                    data-sanity={encodeDataAttribute?.('duration.publishedAt')}
-                  >
-                    {year}
-                  </span>
+              <div className="divide-inherit grid grid-cols-1 divide-y lg:grid-cols-4 lg:divide-x lg:divide-y-0">
+                {/* Tags */}
+                <div className="p-3 lg:p-4">
+                  <div className="text-xs md:text-sm">Tags</div>
+                  <div className="text-md flex flex-row flex-wrap md:text-lg">
+                    {tags?.map((tag, key) => (
+                      <div key={key} className="mr-1 break-words ">
+                        #{tag}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            )}
-
-            {/* Tags */}
-            <div className="p-3 lg:p-4">
-              <div className="text-xs md:text-sm">Tags</div>
-              <div className="text-md flex flex-row flex-wrap md:text-lg">
-                {tags?.map((tag, key) => (
-                  <div key={key} className="mr-1 break-words ">
-                    #{tag}
-                  </div>
-                ))}
-              </div>
             </div>
+
+            {/* Title */}
+            <h1 className={styles.title}>{title}</h1>
+
+            {/* Description */}
+            {description && (
+              <CustomPortableText
+                paragraphClasses={styles.description}
+                value={description}
+              />
+            )}
           </div>
         </div>
-
-        {/* Description */}
-        {description && (
-          <CustomPortableText
-            paragraphClasses="font-serif max-w-3xl text-xl text-gray-600"
-            value={description}
-          />
-        )}
+        <Link className={styles.posts_page_link} href="/posts">
+          Back To Posts
+        </Link>
       </div>
-      <Link className="px-16" href="/posts">
-        Back To Posts
-      </Link>
+
       <div className="absolute left-0 w-screen border-t" />
     </div>
   )
